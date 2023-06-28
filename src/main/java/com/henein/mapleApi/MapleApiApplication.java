33f6cbb8c9ba59ab2cb3f6690399adaca7810bb3
DESKTOP-J0ABEDB\이명재 <love12mj@naver.com>
2023-06-28 18:38:30 +0900

    db없이 실행하도록 변경

--- src/main/java/com/henein/mapleApi/MapleApiApplication.java
+++ src/main/java/com/henein/mapleApi/MapleApiApplication.java
@@ -2,10 +2,11 @@ package com.henein.mapleApi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class MapleApiApplication {
 
 	public static void main(String[] args) {
