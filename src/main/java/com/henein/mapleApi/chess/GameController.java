5e321aaefac04bf45f86a90db17720ff1242e5ca
DESKTOP-J0ABEDB\이명재 <love12mj@naver.com>
2023-09-14 23:00:23 +0900

    Add hello

--- src/main/java/com/henein/mapleApi/chess/GameController.java
+++ src/main/java/com/henein/mapleApi/chess/GameController.java
@@ -1,6 +1,7 @@
 package com.henein.mapleApi.chess;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -9,14 +10,17 @@ import java.util.UUID;
 @RestController
 @RequestMapping("/game")
 @RequiredArgsConstructor
+@Slf4j
 public class GameController {
     private final GameMainService gameMainService;
     @GetMapping("/roomset")
     public String roomSet(){
+        log.info("들어옴");
         return String.valueOf(UUID.randomUUID());
     }
     @PostMapping("/init")
     public PieceInfoDto[][] gameSetting(@RequestHeader("Room")String roomId){
+        log.info("init 들어옴"+roomId);
         return gameMainService.setting2DChessBoard(roomId);
     }
 //    @GetMapping("/piece/valid-list")
